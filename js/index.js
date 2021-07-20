bf46b5db545c6fdd336adb16e5e2610e79d768fd
15766372458 <201186668@qq.com>
2021-07-20 17:11:37 +0800

    初始化

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+// function getMaxZIndex() {
+//     var maxZ = Math.max.apply(null, 
+// 　　    $.map($('body *'), function(e,n) {
+//   　　  if ($(e).css('position') != 'static')
+//     　　    return parseInt($(e).css('z-index')) || -1;
+//     }));
+//     return maxZ;
+// }
+// getMaxZIndex();
